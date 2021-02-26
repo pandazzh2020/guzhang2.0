bae0bb5d9a12039de4403418fa353e087ff2e8c5
zhengpanda <ridethewind2020@163.com>
2021-02-26 13:24:18 +0800

    2021.2.16

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+	<router-view></router-view>
+  </div>
+</template>
+
+<script>
+import index from './components/index.vue'
+
+export default {
+  name: 'App',
+  components: {
+    'index': index
+  }
+}
+</script>
+
+<style>
+#app {
+   height: 100%;
+}
+</style>
